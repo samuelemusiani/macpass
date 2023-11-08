f45422f07a9f39d69887605dc4165ed929c7eb91
Samuele Musiani <samu@teapot.ovh>
2023-11-08 13:53:02 +0100

    logging on macscan

--- pkg/macscan/macscan.go
+++ pkg/macscan/macscan.go
@@ -2,8 +2,7 @@ package macscan
 
 import (
 	"encoding/binary"
-	// "fmt"
-	"log"
+	"log/slog"
 	"net"
 	"time"
 
@@ -15,38 +14,33 @@ type IpMac struct {
 	Mac net.HardwareAddr
 }
 
-//	func main() {
-//		r := ScanSubnet(net.IPNet{IP: net.IPv4(130, 136, 3, 1), Mask: net.IPv4Mask(255, 255, 255, 0)}, time.Second*4, 200)
-//		for _, i := range r {
-//			fmt.Println(i)
-//		}
-//	}
-
 func ScanSubnet(subnet net.IPNet, timeout time.Duration, threads uint8) []IpMac {
+	slog.With("subnet", subnet, "timeout", timeout, "threads", threads).Info("Scanning subnet")
+
 	hosts := make(chan net.IP, 255)
 	results := make(chan IpMac, 255)
 	done := make(chan bool, threads)
 
 	// Start workers
-	log.Println("Starting workers")
+	slog.Debug("Starting workers")
 	for worker := uint8(0); worker < threads; worker++ {
 		go arpingHosts(hosts, results, done, timeout)
 	}
 
 	// Iterate over all the subnet and send hosts to hosts channel
-	log.Println("Generating ips")
+	slog.Debug("Generating ips")
 	for i, ip := bBu(subnet.Mask), bBu(subnet.IP.Mask(subnet.Mask))+1; i < ^uint32(0)-1; i, ip = i+1, ip+1 {
 		var tmp []byte = make([]byte, 4)
 		bBp(tmp, uint32(ip))
 		// tmpip := make([]byte, 4)
 		// copy(tmpip, tmp) // Need to copy because it's an array
+		slog.Debug("Generated ip: ", net.IP(tmp).String())
 		hosts <- tmp //Segmentation fault? Where does the scope of tmp ends?
 	}
 	close(hosts)
 
-	// collect responses
+	slog.Debug("Collecting responses from workers")
 	var responses []IpMac
-	log.Println("Collecting responses")
 	for {
 		select {
 		case found := <-results:
@@ -54,6 +48,7 @@ func ScanSubnet(subnet net.IPNet, timeout time.Duration, threads uint8) []IpMac 
 		case <-done:
 			threads--
 			if threads == 0 {
+				slog.Debug("All worker have finished")
 				close(results)
 				for i := range results {
 					responses = append(responses, i)
@@ -65,22 +60,21 @@ func ScanSubnet(subnet net.IPNet, timeout time.Duration, threads uint8) []IpMac 
 }
 
 func arpingHosts(hosts chan net.IP, repondingHosts chan IpMac, done chan bool, timeout time.Duration) {
-	// log.Println("Worker started")
+	slog.Debug("Worker started")
 	arping.SetTimeout(timeout)
 
 	for host := range hosts {
-		// log.Println("Make arping to ", host)
 		mac, _, err := arping.Ping(host)
 		if err != nil {
-			// log.Println("Error during arping to host: ", host)
-			// log.Println(err)
+			slog.With("error", err, "host", host).Debug("Error during arping to host")
 		} else {
-			// log.Println("Host responded with mac: ", mac)
+			slog.With("host", host, "mac", mac).Debug("Host responded ", mac)
 			repondingHosts <- IpMac{Ip: host, Mac: mac}
 		}
 	}
 
 	done <- true
+	slog.Debug("Worker have finished")
 }
 
 // Alias for binary.BigEndian.Uint32(a)
