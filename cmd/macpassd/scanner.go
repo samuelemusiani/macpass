fc10cb50b7cd8af8822bbbb9bc975507f3550a6a
Samuele Musiani <samu@teapot.ovh>
2023-11-28 00:48:56 +0100

    arp table instead of network scanning

--- cmd/macpassd/scanner.go
+++ cmd/macpassd/scanner.go
@@ -1,28 +1,73 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"log/slog"
 	"net"
-	"time"
+	"os"
+	"reflect"
+	"strings"
 
-	"github.com/musianisamuele/macpass/cmd/macpassd/config"
 	"github.com/musianisamuele/macpass/cmd/macpassd/registration"
-	"github.com/musianisamuele/macpass/pkg/macscan"
 )
 
 func scanNetwork() {
-	conf := config.Get()
-	ip := net.ParseIP(conf.Network.Ip)
-	mask := net.IPMask(net.ParseIP(conf.Network.Mask).To4())
-	timeout := time.Millisecond * time.Duration(conf.Network.Timeout)
-	workers := conf.Network.Workers
-
-	slog.With("ip", ip, "mask", mask).Debug("scanNetwork")
-
-	r := macscan.ScanSubnet(net.IPNet{IP: ip, Mask: mask}, timeout, workers)
-
-	slog.Debug("Adding ScanSubnet responses")
-	for _, i := range r {
-		registration.AddIpToMac(i.Ip, i.Mac)
+	// get arptable
+	path := "/proc/net/arp"
+	arpTable, err := os.ReadFile(path)
+	if err != nil {
+		slog.With("path", path, "err", err).Error("Can't read arp table. No logs can be provided for network devices")
 	}
+
+	emptyMac, _ := net.ParseMAC("00:00:00:00:00:00")
+	line := []byte{}
+
+	hwPos := -1
+	for _, data := range arpTable {
+		if !bytes.Equal([]byte{data}, []byte("\n")) {
+			line = append(line, data)
+		} else {
+			if hwPos == -1 {
+				hwPos = strings.Index(string(line), "HW address")
+				if hwPos == -1 {
+					slog.With("line", string(line), "substring", "HW address").
+						Error("Substring cannot be found. Arp tables is not right")
+					break
+				}
+			} else {
+				ip, mac, err := parseArpLine(line, hwPos)
+				if err != nil {
+					slog.With("line", string(line), "err", err).
+						Error("Error parsing arp line")
+				} else if !reflect.DeepEqual(mac.String(), emptyMac.String()) {
+					registration.AddIpToMac(ip, mac)
+				}
+			}
+			line = line[:0]
+		}
+	}
+}
+
+// line is the all line of the arp table
+// hwPos the the number of the first byte that contains the mac address
+func parseArpLine(line []byte, hwPos int) (net.IP, net.HardwareAddr, error) {
+	l := len(line)
+	if hwPos >= l {
+		return nil, nil, fmt.Errorf("hwPos is greater than the line length")
+	}
+	if hwPos <= 0 {
+		return nil, nil, fmt.Errorf("hwPos is too small")
+	}
+
+	var ip net.IP
+	if line[11] == byte(' ') {
+		ip = net.ParseIP(string(line[0:11]))
+	} else {
+		ip = net.ParseIP(string(line[0:12]))
+	}
+
+	mac, err := net.ParseMAC(string(line[hwPos : hwPos+17]))
+
+	return ip, mac, err
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/ghodss/yaml v1.0.0
 	github.com/j-keck/arping v1.0.3
 	github.com/jcmturner/gokrb5/v8 v8.4.4
-	github.com/spf13/viper v1.17.0
+	github.com/mattn/go-sqlite3 v1.14.17
 	golang.org/x/term v0.13.0
 	gotest.tools/v3 v3.5.1
 	internal/comunication v1.0.0
@@ -16,33 +16,19 @@ require (
 replace internal/comunication => ./internal/comunication
 
 require (
-	github.com/fsnotify/fsnotify v1.6.0 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
 	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
 	github.com/jcmturner/gofork v1.7.6 // indirect
 	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
-	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/mattn/go-sqlite3 v1.14.17 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
-	github.com/sagikazarmark/locafero v0.3.0 // indirect
-	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
-	github.com/sourcegraph/conc v0.3.0 // indirect
-	github.com/spf13/afero v1.10.0 // indirect
-	github.com/spf13/cast v1.5.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/subosito/gotenv v1.6.0 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.9.0 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	golang.org/x/crypto v0.13.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.15.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 	gopkg.in/yaml.v2 v2.2.2 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
