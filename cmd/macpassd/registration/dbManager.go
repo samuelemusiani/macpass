17bf4cae2ca213ac138e56c92f12eadb4587cf50
Samuele Musiani <samu@teapot.ovh>
2023-12-30 00:47:09 +0100

    added ips in database, refactoring

--- cmd/macpassd/registration/dbManager.go
+++ cmd/macpassd/registration/dbManager.go
@@ -2,7 +2,9 @@ package registration
 
 import (
 	"database/sql"
-	"log"
+	"fmt"
+	"log/slog"
+	"net"
 	"os"
 	"strings"
 	"time"
@@ -10,72 +12,156 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func readRegInstances(row *sql.Rows) []Registration {
-	var macs []Registration
-	var current Registration
-	for row.Next() {
-		row.Scan(&current.Id, &current.User, &current.Mac, &current.Start, &current.End, &current.IsDown)
-		macs = append(macs, current)
-	}
-	return macs
-}
+func connectDB(dbPath string) *sql.DB {
+	slog.Info("Connecting to DB")
 
-func getActive(con *sql.DB) []Registration {
-	selectOutdated := `select * from Log where ? < endTime and isDown = false`
-	row, err := con.Query(selectOutdated, time.Now())
-	if err != nil {
-		log.Fatal(err)
-	}
-	return readRegInstances(row)
-}
+	// If the path is prefixed with ":" the db is created in memory. This helps
+	// on tests
+	if !strings.HasPrefix(dbPath, ":") {
+		_, err := os.Stat(dbPath)
 
-func getOutdated(con *sql.DB) []Registration {
-	selectOutdated := `select * from Log where ? > endTime and isDown = false`
-	row, err := con.Query(selectOutdated, time.Now())
-	if err != nil {
-		log.Fatal(err)
-	}
-	return readRegInstances(row)
-}
+		if os.IsNotExist(err) {
+			slog.With("dbPath", dbPath).
+				Info("Creating db as not present in the path specified")
 
-func setOutdated(con *sql.DB, macs []Registration) {
-	for i := range macs {
-		id := macs[i].Id
-		updateOutdated := `update Log set isDown = true where id = ?`
-		con.Exec(updateOutdated, id)
-		macs[i].IsDown = true
-	}
-}
-
-func insertRegistration(db *sql.DB, reg Registration) {
-	const insertLog = `insert into Log(User, MAC, startTime, endTime, isDown) values(?, ?, ?,? , false)`
-	_, err := db.Exec(insertLog, reg.User, reg.Mac, reg.Start, reg.End)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-}
-
-func connectDB(sqlFile string) *sql.DB {
-	log.Println("Connecting to DB")
-	const createLogTable = `create table if not exists 
-	Log(id integer primary key autoincrement,User varchar(255), MAC varchar(17),startTime datetime, endTime datetime, isDown bool)
-	`
-	if !strings.HasPrefix(sqlFile, ":") {
-		if _, err := os.Stat(sqlFile); os.IsNotExist(err) {
-			os.Create(sqlFile)
+			_, err := os.Create(dbPath)
+			if err != nil {
+				slog.With("dbPath", dbPath).Error("Creating database file")
+				return nil
+			}
+			slog.With("dbPath", dbPath).Info("Database file created")
+		} else {
+			slog.With("dbPath", dbPath).Info("Databse file is already present in path")
 		}
 	}
-	db, err := sql.Open("sqlite3", sqlFile)
+
+	db, err := sql.Open("sqlite3", dbPath)
 
 	if err != nil {
-		log.Fatal(err)
+		slog.With("dbPath", dbPath, "db", db, "err", err).
+			Error("Opening database")
+
+		return nil
 	}
 
-	_, err = db.Exec(createLogTable)
+	const query = `create table if not exists Log(id integer primary key 
+  autoincrement, User varchar(255), MAC varchar(17), IPs blob, 
+  startTime datetime, endTime datetime, isDown bool)`
+
+	_, err = db.Exec(query)
 	if err != nil {
-		log.Fatal(err)
+		slog.With("db", db, "query", query, "err", err).
+			Error("Creating table on databese. Databse can't be used")
+
+		return nil
 	}
-	log.Println("Connected to DB")
+
+	slog.Info("Successfully connected to db")
 	return db
+}
+
+// This function change the value "isDown" to true for every items in the
+// db that match the ids of the items in r
+func setOutdated(db *sql.DB, r []Registration) {
+	for i := range r {
+		query := `update Log set isDown = true where id = ?`
+
+		_, err := db.Exec(query, r[i].Id)
+		if err != nil {
+			slog.With("db", db, "query", query, "reg", r[i], "err", err)
+		}
+	}
+}
+
+func insertRegistration(db *sql.DB, r Registration) {
+	const query = `insert into Log(User, MAC, IPs, startTime, endTime, isDown) values(?, ?, ?, ?, ?, false)`
+	_, err := db.Exec(query, r.User, r.Mac, convertIPsToBytes(r.Ips), r.Start, r.End)
+	if err != nil {
+		slog.With("db", db, "query", query, "registration", r, "err", err).
+			Error("Inserting registration into database")
+	}
+}
+
+func getActive(db *sql.DB) []Registration {
+	selectOutdated := `select * from Log where ? < endTime and isDown = false`
+	row, err := db.Query(selectOutdated, time.Now())
+	if err != nil {
+		slog.With("db", db, "err", err).
+			Error("During query for active registrations on db")
+
+		return make([]Registration, 0)
+	}
+	return parseRows(row)
+}
+
+// Return all registrations that are outdated based on endTime, BUT NOT FROM THE
+// "isDown" value. After this, the function setOutdated(...) should be called.
+func getOutdated(db *sql.DB) []Registration {
+	selectOutdated := `select * from Log where ? > endTime and isDown = false`
+	row, err := db.Query(selectOutdated, time.Now())
+	if err != nil {
+		slog.With("db", db, "err", err).
+			Error("During query for outdated registrations on db")
+
+		return make([]Registration, 0)
+	}
+	return parseRows(row)
+}
+
+func parseRows(rows *sql.Rows) (r []Registration) {
+	for rows.Next() {
+		var tmp Registration
+		tmpIps := make([]byte, 0)
+		rows.Scan(&tmp.Id, &tmp.User, &tmp.Mac, &tmpIps, &tmp.Start, &tmp.End,
+			&tmp.IsDown)
+
+		var err error
+		tmp.Ips, err = convertBytesToIPs(tmpIps)
+		if err != nil {
+			fmt.Println(tmpIps)
+			slog.With("bytes", tmpIps, "err", err).
+				Error("Could not parse ips from bytes blob extracted from database")
+
+			tmp.Ips = nil
+		}
+
+		r = append(r, tmp)
+	}
+	return r
+}
+
+func convertIPsToBytes(ips []net.IP) []byte {
+	blob := make([]byte, 16*len(ips))
+
+	for i := range ips {
+		tmp := []byte(ips[i])
+
+		for j := 0; j < 16; j++ {
+			blob[i*16+j] = tmp[j]
+		}
+	}
+
+	return blob
+}
+
+func convertBytesToIPs(blob []byte) ([]net.IP, error) {
+	l := len(blob)
+
+	if l%16 != 0 {
+		return nil, fmt.Errorf("Could not convert []byte to []net.IP. Lenght is not divisible by 16. length: %d", l)
+	}
+
+	ips := make([]net.IP, l/16)
+
+	for i := 0; i < l; i += 16 {
+		tmp := make([]byte, 16)
+
+		for j := 0; j < 16; j++ {
+			tmp[j] = blob[i+j]
+		}
+
+		ips[i%16] = net.IP(tmp)
+	}
+
+	return ips, nil
 }
--- cmd/macpassd/registration/dbManager_test.go
+++ cmd/macpassd/registration/dbManager_test.go
@@ -1,6 +1,7 @@
 package registration
 
 import (
+	"net"
 	"os"
 	"testing"
 	"time"
@@ -9,7 +10,7 @@ import (
 )
 
 func TetsConnecting(t *testing.T) {
-	test := "test.sql"
+	test := "grfgQngnonfrJvguFrpergAnzr.sql"
 	db := connectDB(test)
 	defer db.Close()
 	os.Remove(test)
@@ -17,36 +18,30 @@ func TetsConnecting(t *testing.T) {
 
 const MemoryDB = ":memory:"
 
-func checkEqualMacRegistration(t *testing.T, macReg1, macReg2 Registration) {
-	assert.Equal(t, macReg1.Mac, macReg2.Mac)
-	assert.Equal(t, macReg1.User, macReg2.User)
-	assert.Equal(t, macReg1.End.In(time.UTC), macReg2.End.In(time.UTC))
-	assert.Equal(t, macReg1.Start.Unix(), macReg2.Start.Unix())
-}
-
 func TestSetOutdated(t *testing.T) {
 	db := connectDB(MemoryDB)
 	defer db.Close()
-	hour, _ := time.ParseDuration("1h")
-	ms, _ := time.ParseDuration("1ms")
+
+	hour, ms := time.Hour, time.Millisecond
 	insertRegistration(db, Registration{Id: 100, User: "user0",
-		Mac: "08:7d:bb:7a:cb:d0", Start: time.Now(), End: time.Now().Add(hour),
-		IsDown: false})
+		Mac: "08:7d:bb:7a:cb:d0", Ips: []net.IP{net.IPv4(1, 2, 3, 4)},
+		Start: time.Now(), End: time.Now().Add(hour), IsDown: false})
 	insertRegistration(db, Registration{Id: 101, User: "user1",
-		Mac: "80:57:61:7e:d1:dd", Start: time.Now(), End: time.Now().Add(hour),
-		IsDown: false})
+		Mac: "80:57:61:7e:d1:dd", Ips: []net.IP{net.IPv4(5, 6, 7, 8)},
+		Start: time.Now(), End: time.Now().Add(hour), IsDown: false})
 
 	user2 := Registration{Id: 102, User: "user2",
-		Mac: "fb:65:ee:13:76:af", Start: time.Now(), End: time.Now().Add(ms),
-		IsDown: false}
+		Mac: "fb:65:ee:13:76:af", Ips: []net.IP{net.IPv4(122, 245, 1, 75)},
+		Start: time.Now(), End: time.Now().Add(ms), IsDown: false}
 
 	time.Sleep(2 * ms)
 	insertRegistration(db, user2)
 
-	macs := getOutdated(db)
-	setOutdated(db, macs)
-	assert.Equal(t, len(macs), 1)
-	user3out := macs[0]
+	r := getOutdated(db)
+	setOutdated(db, r)
+	assert.Equal(t, len(r), 1)
+
+	user3out := r[0]
 	checkEqualMacRegistration(t, user2, user3out)
 }
 
@@ -57,17 +52,24 @@ func TestGetActive(t *testing.T) {
 	ms, _ := time.ParseDuration("1ms")
 
 	insertRegistration(db, Registration{Id: 100, User: "user0",
-		Mac: "08:7d:bb:7a:cb:d0", Start: time.Now(), End: time.Now().Add(hour),
-		IsDown: false})
+		Mac: "08:7d:bb:7a:cb:d0", Ips: []net.IP{net.IPv4(1, 2, 3, 4)},
+		Start: time.Now(), End: time.Now().Add(hour), IsDown: false})
 	insertRegistration(db, Registration{Id: 101, User: "user1",
-		Mac: "80:57:61:7e:d1:dd", Start: time.Now(), End: time.Now().Add(hour),
-		IsDown: false})
+		Mac: "80:57:61:7e:d1:dd", Ips: []net.IP{net.IPv4(5, 6, 7, 8)},
+		Start: time.Now(), End: time.Now().Add(hour), IsDown: false})
 
 	insertRegistration(db, Registration{Id: 102, User: "user2",
-		Mac: "fb:65:ee:13:76:af", Start: time.Now(), End: time.Now().Add(ms),
-		IsDown: false})
+		Mac: "fb:65:ee:13:76:af", Ips: []net.IP{net.IPv4(122, 245, 1, 75)},
+		Start: time.Now(), End: time.Now().Add(ms), IsDown: false})
 
 	time.Sleep(2 * ms)
 	macs := getActive(db)
 	assert.Equal(t, len(macs), 2)
+}
+
+func checkEqualMacRegistration(t *testing.T, macReg1, macReg2 Registration) {
+	assert.Equal(t, macReg1.Mac, macReg2.Mac)
+	assert.Equal(t, macReg1.User, macReg2.User)
+	assert.Equal(t, macReg1.End.Round(time.Second).Unix(), macReg2.End.Round(time.Second).Unix())
+	assert.Equal(t, macReg1.Start.Round(time.Second).Unix(), macReg2.Start.Round(time.Second).Unix())
 }
